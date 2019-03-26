ff877d06b7e7964b7dbcfbdcbbd0dba9161ed61c
Bram van Rhee <123575@student.drenthecollege.nl>
2019-03-26 09:30:42 +0100

    Gemiddelde berekenen

--- JavaLevel1.java
+++ JavaLevel1.java
@@ -5,14 +5,18 @@ public class JavaLevel1 {
 		int rowCount = Integer.parseInt(args[0]); //Hierdoor kun je invullen hoeveel rows er op je scherm verschijnen
 		int columnCount = Integer.parseInt(args[1]); //Hierdoor kun je invullen hoeveel columns er op je scherm verschijnen
 		int [][] getallen = new int[rowCount][columnCount];
+		int[] gemiddelde = new int[rowCount]; 
 		
-		//Generen random getallen
+		//Generen random getallen en rekent het gemiddelde uit
 		for (int row = 0; row < rowCount; row++) {
 			int som = 0;
 			for (int col = 0; col < columnCount; col++) {
 				getallen[row][col] = (int) Math.ceil(Math.random() * 100);
 				som += getallen[row][col];
 			}
+			gemiddelde[row] = som/rowCount;
+			
+			
 		}
 	}
 
